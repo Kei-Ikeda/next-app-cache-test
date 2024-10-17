7494ebcb3c810c1322b74aaaa38a34f137ba0b78
Kei-Ikeda <g9it56@gmail.com>
2024-10-18 07:05:02 +0900

    fix page annotation text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,17 @@ export default function Home() {
       <p className="font-bold">
         ポケモンが再レンダリングされる場合はサーバー側でAPIからの再取得が発生しています
       </p>
+      <p className="text-sm mt-4">
+        ※ Amplify の
+        <a
+          href="https://github.com/aws-amplify/amplify-hosting/issues/3843"
+          target="_blank"
+          className="cursor-pointer text-blue-800 underline font-bold"
+        >
+          仕様
+        </a>
+        で Suspense の fallback が動作していません
+      </p>
       <div className="mt-4 flex flex-col gap-y-2">
         <p>-------SSG-------</p>
         <Suspense fallback={<>Loading...</>}>
@@ -32,7 +43,7 @@ export default function Home() {
           <Isr />
         </Suspense>
         <p className="text-sm">
-          初回レンダリングまたは前回のリロードから20秒以上経過後のリロード時、
+          初回レンダリング及び前回のリロードから20秒以上経過後のリロード時、
           またはキャッシュクリアボタン押下後の画面リロードでサーバー側でAPIから再取得
         </p>
         <div className="max-w-[340px]">
